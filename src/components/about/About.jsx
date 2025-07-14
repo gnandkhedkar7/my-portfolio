a524cc2ef322e891b36d0533eafce114cc63b3a2
gnandkhedkar7 <nandkhedkargauri99@gmail.com>
2025-07-14 13:42:30 +0200

    feat: added the about section with styling

---
+++ src/components/about/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src="" alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src="" alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am an experienced Frontend DEveloper with over more than 3 years
+              of experience
+            </p>
+            <p>My passion for frontend developement is not only</p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>REact JS</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{ width: "75%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Next JS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+          </div>
+        </div>
+        <div className="about-achievements">
+            <div className="achievement">
+                <h1>3+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div>
+            <hr />
+            <div className="achievement">
+                <h1>5+</h1>
+                <p>PROJECTS COMPLETED</p>
+            </div>
+            <hr />
+            <div className="achievement">
+                <h1>15+</h1>
+                <p>HAPPY CLIENTS</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
